d43b29d0dd3ee241178bedfa657b0d9d16d1f78b
Midhet Sulemani <midhetfatema94@gmail.com>
2017-04-18 19:17:30 +0530

    made connector

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,16 +1,24 @@
 import { React, Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
-
 
 class LibraryList extends Component {
     
+    componentWillMount () {
+        
+    }
+
     render() {
-        return;
+        console.log(this.props);
+        return (
+            <View></View>
+        );
     }
 }
 
 const mapStatetoProps = state => {
     console.log(state);
+    return { libraries: state.libraries };
 };
 
-export default connect()(LibraryList);
+export default connect(mapStatetoProps)(LibraryList);
